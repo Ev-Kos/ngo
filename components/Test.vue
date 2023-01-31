205ac20851917857f7b3838ddc87b6ffbc506856
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-01-31 10:19:43 +0300

    создание тестового однофайлового компонента

---
+++ components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Hello world</p>
+    <label>
+      Input field
+      <input class="input" v-model="testData"/>
+    </label>
+    <p class="result">{{ testData }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        testData: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .input {
+    width: 100px;
+    height: 30px;
+    border: 1px solid red;
+  }
+  .result {
+    color: blue;
+  }
+</style>
