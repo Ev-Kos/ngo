04c2d3b01563c55affdaeb453718f76df5c1a2e6
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-01-31 12:50:14 +0300

    error layout

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error-block">
+    <h3>Ой-ой! У нас ошибка, но все под контролем :)</h3>
+    <h3>Статус: {{ error.statusCode }}</h3>
+    <h3>Сообщение об ошибке: {{ error.message }}</h3>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      error: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    mounted() {
+      console.log(this.error, 'ERROR OBJECT')
+    }
+  }
+</script>
+
+<style scoped>
+  .error-block {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+  }
+</style>
